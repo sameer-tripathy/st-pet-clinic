21c689e40a3ac3593fd67765970994d0d903d22a
samee <samee@DESKTOP-9O54JP3>
2019-01-06 18:35:51 +0530

    First Commit

---
+++ st-pet-clinic/src/main/java/sameer/springframework/stpetclinic/StPetClinicApplication.java
@@ -0,0 +1,14 @@
+package sameer.springframework.stpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StPetClinicApplication.class, args);
+	}
+
+}
+
---
+++ st-pet-clinic/src/test/java/sameer/springframework/stpetclinic/StPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package sameer.springframework.stpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
