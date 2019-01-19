55d999c3d7c7620d2536d5228d60306b852a6b4d
samee <samee@DESKTOP-9O54JP3>
2019-01-19 09:04:20 +0530

    Corrected the error. Closes #13

--- st-pet-clinic/pet-clinic-web/src/main/java/sameer/springframework/stpetclinic/controllers/OwnerController.java
+++ st-pet-clinic/pet-clinic-web/src/main/java/sameer/springframework/stpetclinic/controllers/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 	
-	@RequestMapping({"", "/", "/owners/index", "/owners/index.html"})
+	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String listOwners() {
 		return "owners/index";
 	}
