b8012769d375908152243e568f2e18ddedf581a3
samee <samee@DESKTOP-9O54JP3>
2019-01-20 07:33:49 +0530

    Updated Vet controller and added the display logic on vet index page. Closes #15

--- st-pet-clinic/pet-clinic-web/src/main/java/sameer/springframework/stpetclinic/controllers/VetController.java
+++ st-pet-clinic/pet-clinic-web/src/main/java/sameer/springframework/stpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package sameer.springframework.stpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import sameer.springframework.stpetclinic.services.VetService;
 
 @Controller()
 @RequestMapping("vets")
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		System.out.println(vetService.findAll());
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
