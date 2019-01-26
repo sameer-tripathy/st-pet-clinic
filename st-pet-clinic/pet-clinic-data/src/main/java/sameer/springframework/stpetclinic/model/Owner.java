c35666de1a0bc7f15fa74469500c6826bbee1289
samee <samee@DESKTOP-9O54JP3>
2019-01-26 07:16:55 +0530

    Added contact info properties to Owner. Close #28

--- st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/model/Owner.java
+++ st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/model/Owner.java
@@ -6,7 +6,34 @@ public class Owner extends Person {
 	
 	private static final long serialVersionUID = 1165024733603576345L;
 	
+	private String address;
+	private String city;
+	private String telephone;
 	Set<Pet> pets;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
