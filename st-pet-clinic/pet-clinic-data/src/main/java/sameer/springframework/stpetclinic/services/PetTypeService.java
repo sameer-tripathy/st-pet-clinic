7509da10a86d04a032329fed0fa1b9eba6a9641e
samee <samee@DESKTOP-9O54JP3>
2019-01-26 07:44:06 +0530

    Created PetType Map Service Impl. Close #45

---
+++ st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package sameer.springframework.stpetclinic.services;
+
+import sameer.springframework.stpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package sameer.springframework.stpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import sameer.springframework.stpetclinic.model.PetType;
+import sameer.springframework.stpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+}
