4570c87e0a269a46ac8034b4f858e9797b2cc674
samee <samee@DESKTOP-9O54JP3>
2019-09-18 20:02:29 +0530

    Updated baseEntity with jJPA annotation. Closes #35

--- st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/model/BaseEntity.java
+++ st-pet-clinic/pet-clinic-data/src/main/java/sameer/springframework/stpetclinic/model/BaseEntity.java
@@ -2,10 +2,18 @@ package sameer.springframework.stpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 	
 	private static final long serialVersionUID = -7547450199930984012L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
